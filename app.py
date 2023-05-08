a3cb4578a0bd22e0e5f8429c24783a4b22017e22
Dipanshu Diwaker <56470089+dipanshudiwaker@users.noreply.github.com>
2023-05-08 16:46:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ def home():
 def chat():
     context = [ {'role':'system', 'content':"""
     Your name is Adam from LTTS Company. Your service is to collect queries related to LTTS Company only.If the query is not related to LTTS Company, say 'no data found'. \
+    give Summerized  Answer all ways in 10 words. \
     First, greet the client and ask for their query related to LTTS Company. \
-    Summerize the Answer in 10 words and give in list way. \
     Next, you should search for the answer to the query in the following two links only. \
     https://ltts.com/ ltts official site. \
     https://en.wikipedia.org/wiki/ltts. \
